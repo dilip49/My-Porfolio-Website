42b9875f2547060c107b5e36650bc5f115d00c76
dilip49 <dilip.bhaidiya@gmail.com>
2023-03-18 15:45:58 +0530

    First Release with initial configurations

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Hi there, I'm Dilip, a Full-Stack Developer with 4+ years of experience in Software development. I specialize in Ruby, Ruby on rails, React js, Javascript, MySQL, PostgreSQL, GIT, AWS, Google Cloud, HTML/CSS, Docker, Kubernetes and have worked on projects for clients in various industries, including e-commerce, artwork marketplace, social networking sites and digit solutions.
+
+        Throughout my career, I've honed my skills in ruby on rails and react js and have developed a strong understanding of Ruby, Ruby on rails, React js and MySQL. I'm always eager to learn more and stay up-to-date with the latest trends and best practices in my field.
+
+        When I'm not working, I enjoy cooking, coding and watching online content. 
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
