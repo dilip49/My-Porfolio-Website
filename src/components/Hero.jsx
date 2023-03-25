dc8ee55b357de474ac542a81264f9280a72c983a
dilip49 <dilip.bhaidiya@gmail.com>
2023-03-25 17:06:15 +0530

    [Update] bio text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Dilip</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I web applications and user <br className="sm:block hidden" />
-            interfaces using ROR and React JS.
+            Web developer with 4+ years of experience <br className="sm:block hidden" /> creating responsive and user-friendly web applications. 
           </p>
         </div>
       </div>
