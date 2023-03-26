bc5867ff3a02d1417ec0c75e891bdd558065fc0f
Dilip Bhaidiya <dilip.bhaidiya@gmail.com>
2023-03-26 23:42:13 +0530

    feature/configure netlify form to get details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,55 +8,55 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const [loading, setLoading] = useState(false);
+  // const formRef = useRef();
+  // const [form, setForm] = useState({
+  //   name: "",
+  //   email: "",
+  //   message: "",
+  // });
+  // const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
-  };
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setForm({ ...form, [name]: value });
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setLoading(true);
 
-    emailjs
-      .send(
-        "service_r0jcplm",
-        "template_1t76uxq",
-        {
-          form_name: form.name,
-          to_name: "Liron",
-          from_email: form.email,
-          to_email: "contact@mail.com",
-          message: form.message,
-        },
-        "Jqq9AvwIuSjoMiA5c"
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+  //   emailjs
+  //     .send(
+  //       "service_r0jcplm",
+  //       "template_1t76uxq",
+  //       {
+  //         form_name: form.name,
+  //         to_name: "Liron",
+  //         from_email: form.email,
+  //         to_email: "contact@mail.com",
+  //         message: form.message,
+  //       },
+  //       "Jqq9AvwIuSjoMiA5c"
+  //     )
+  //     .then(
+  //       () => {
+  //         setLoading(false);
+  //         alert("Thank you. I will get back to you as soon as possible.");
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
+  //         setForm({
+  //           name: "",
+  //           email: "",
+  //           message: "",
+  //         });
+  //       },
+  //       (error) => {
+  //         setLoading(false);
 
-          console.log(error);
-          alert("Something went wrong.");
-        }
-      );
-  };
+  //         console.log(error);
+  //         alert("Something went wrong.");
+  //       }
+  //     );
+  // };
 
   return (
     <div
@@ -66,12 +66,13 @@ const Contact = () => {
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <p className={styles.sectionSubText}>Reach out to me</p>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
 
         <form
-          ref={formRef}
-          onSubmit={handleSubmit}
+          name="contact"
+          method="POST"
+          data-netlify="true"
           className="mt-12 flex flex-col gap-8"
         >
           <label className="flex flex-col">
@@ -79,8 +80,6 @@ const Contact = () => {
             <input
               type="text"
               name="name"
-              value={form.name}
-              onChange={handleChange}
               placeholder="What's your name?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
@@ -90,8 +89,6 @@ const Contact = () => {
             <input
               type="email"
               name="email"
-              value={form.email}
-              onChange={handleChange}
               placeholder="What's your email?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
@@ -101,8 +98,6 @@ const Contact = () => {
             <textarea
               rows={7}
               name="message"
-              value={form.message}
-              onChange={handleChange}
               placeholder="What do you want to say?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
@@ -112,7 +107,7 @@ const Contact = () => {
             type="submit"
             className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
           >
-            {loading ? "Sending..." : "Send"}
+            Send
           </button>
         </form>
       </motion.div>
